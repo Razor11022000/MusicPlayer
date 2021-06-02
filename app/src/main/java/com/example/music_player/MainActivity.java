9a8cb486ac86fece6580074b0030d63fdba242d8
Razor11022000 <mithungfeb2@gmail.com>
2021-06-02 21:37:25 +0530

    null pointer exception resolved

--- app/src/main/java/com/example/music_player/MainActivity.java
+++ app/src/main/java/com/example/music_player/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.music_player;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.Manifest;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.PermissionToken;
@@ -24,7 +28,6 @@ public class MainActivity extends AppCompatActivity {
     String[] items;
     ArrayList<String> arrayList;
     ArrayAdapter<String> adapter;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,13 +38,12 @@ public class MainActivity extends AppCompatActivity {
         runtimePermission();
 
     }
-
-    public void runtimePermission() {
+    public void runtimePermission(){
         Dexter.withContext(this).withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
                 .withListener(new PermissionListener() {
                     @Override
                     public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
-                        // doStuff();
+                        //doStuff();
                         displaySongs();
                     }
 
@@ -56,47 +58,52 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }).check();
     }
-
-    /*public void getMusic() {
+/*
+    public void getMusic(){
         ContentResolver contentResolver = getContentResolver();
         Uri songUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
         Cursor songCursor = contentResolver.query(songUri, null, null, null, null);
 
-        if (songCursor != null && songCursor.moveToFirst()) {
-            int songTitle = songCursor.getColumnIndex(MediaStore.Audio.Media.TITLE);
+        if(songCursor != null && songCursor.moveToFirst()){
+            int songTitle =songCursor.getColumnIndex(MediaStore.Audio.Media.TITLE);
             do {
                 String currentTitle = songCursor.getString(songTitle);
                 arrayList.add(currentTitle);
-            } while (songCursor.moveToNext());
+            } while(songCursor.moveToNext());
         }
-    }*/
 
-    /*public void doStuff() {
+    }
+
+    public void doStuff(){
         listView = findViewById(R.id.ListViewSongs);
         arrayList = new ArrayList<>();
         getMusic();
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arrayList);
         listView.setAdapter(adapter);
-    }*/
+    }
+*/
 
     //method to find the songs from external storage
-    public ArrayList<File> findSong(File file) {
+    public ArrayList<File> findSong(File file){
         ArrayList<File> arrayList = new ArrayList<>();
         File[] files = file.listFiles();
 
         //for loop to check all the files whether its mp3 file or a folder
-        for (File singleFile : files) {
-            //checking whether the file we are getting is a directory or not
-            if (singleFile.isDirectory() && !singleFile.isHidden()) {
-                //If this happens we have to send the whole directory into an array
-                //list again to check for songs inside that folder
-                arrayList.addAll(findSong(singleFile));
-            } else {
-                //we check if the file is a mp3 file or wav file
-                if (singleFile.getName().endsWith(".mp3") && singleFile.getName().endsWith(".wav")) {
-                    //If this happens we will add this file to arrayList
-                    //we are getting a song file here
-                    arrayList.add(singleFile);
+        if (files != null) {
+            for (File singleFile : files){
+                //checking whether the file we are getting is a directory or not
+                if(singleFile.isDirectory() && !singleFile.isHidden()){
+                    //If this happens we have to send the whole directory into an array
+                    //list again to check for songs inside that folder
+                    arrayList.addAll(findSong(singleFile));
+                }
+                else{
+                    //we check if the file is a mp3 file or wav file
+                    if (singleFile.getName().endsWith(".mp3") || singleFile.getName().endsWith(".wav")){
+                        //If this happens we will add this file to arrayList
+                        //we are getting a song file here
+                        arrayList.add(singleFile);
+                    }
                 }
             }
         }
@@ -104,19 +111,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //method to display songs inside the ListView
-    private void displaySongs() {
-        File directory = Environment.getExternalStorageDirectory();
-        final ArrayList<File> mySongs = findSong(directory);
+    private void displaySongs(){
+        final ArrayList<File> mySongs = findSong(Environment.getExternalStorageDirectory());
         items = new String[mySongs.size()];
 
         //create for to store all songs inside the items
-        for (int i = 0; i < mySongs.size(); i++) {
+        for (int i = 0; i<mySongs.size(); i++){
             items[i] = mySongs.get(i).getName();
         }
         //Here we will attach all the items into the ListView that is created
-        ArrayAdapter<String> myAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
 
         //attaching the adapter to ListView
         listView.setAdapter(myAdapter);
     }
+
 }
